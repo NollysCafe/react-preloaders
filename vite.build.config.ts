76089b7c21a0215834fa522235e77fba04bce85a
TheNolle <github@thenolle.com>
2025-03-26 11:58:52 +0100

    chore: bump version to 0.1.2; update build scripts and TypeScript configuration; remove unused styles.d.ts
    Signed-off-by: TheNolle <github@thenolle.com>

--- vite.build.config.ts
+++ vite.build.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: path.resolve(__dirname, 'dist'),
-    emptyOutDir: true,
+    emptyOutDir: false,
     sourcemap: true,
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
